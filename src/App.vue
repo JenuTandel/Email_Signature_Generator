66687be433cf140d3fd020f1c0d08555a92b8f1e
Jinal Tandel <jinal.tandel@1rivet.local>
2023-07-27 17:26:45 +0530

    main-page ui

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
-  <signature-generator></signature-generator>
-  <h1 class="text-primary">Hello</h1>
+  <section class="p-4">
+    <h1 class="main-heading text-center">Email Signature Generator</h1>
+    <signature-generator></signature-generator>
+  </section>
 </template>
 
 <script setup lang="ts">
 import signatureGenerator from "./views/signature-generator.vue";
 </script>
+
+<style lang="scss">
+@import "~@/styles.scss";
+</style>
